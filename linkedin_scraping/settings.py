27e658c1f320948aab2a525b0925550cc5ef4cfe
rao-harsh <96484977+rao-harsh@users.noreply.github.com>
2023-09-22 15:13:47 +0530

    Update settings.py

--- linkedin_scraping/settings.py
+++ linkedin_scraping/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # STATIC_DIR = BASE_DIR / "static"
 # STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/fucking/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
